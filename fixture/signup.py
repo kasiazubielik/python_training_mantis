27b4f881e2ca863fe9ecdb641b0bca359ed9a991
Kasia <kasia@air.hq.e-liberty.org>
2018-01-03 22:00:56 +0100

    Added correct receiving email with registration link (POP3)

--- fixture/signup.py
+++ fixture/signup.py
@@ -19,7 +19,7 @@ class SignupHelper:
 
         wd.get(url)
         wd.find_element_by_name("password").send_keys(password)
-        wd.find_element_by_name("password_confirm").send_keys(username)
+        wd.find_element_by_name("password_confirm").send_keys(password)
         wd.find_element_by_css_selector('input[value="Update User"]').click()
 
     def extract_confirmation_url(self, text):
