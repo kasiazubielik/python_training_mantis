d2aecfb4c65cefafba753dfab1be38c8607518f0
Kasia <kasia@air.hq.e-liberty.org>
2018-01-05 22:38:58 +0100

    Added add and delete project tests with SOAP

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -10,3 +10,15 @@ def test_add_project(app, db):
         old_projects.append(project)
     assert len(old_projects) == len(new_projects)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+
+def test_add_project_with_soap(app):
+    project = Project(name="name4", description="description4")
+    username = "administrator"
+    password = "root"
+    old_projects = app.soap.get_projects(username, password)
+    app.project.create(project)
+    new_projects = app.soap.get_projects(username, password)
+    if project.name != "" and project.name not in [x.name for x in old_projects]:
+        old_projects.append(project)
+    assert len(old_projects) == len(new_projects)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
--- test/test_delete_project.py
+++ test/test_delete_project.py
@@ -11,3 +11,15 @@ def test_delete_project(app, db):
     new_projects = db.get_project_list()
     old_projects.remove(project)
     assert old_projects == new_projects
+
+def test_delete_project_with_soap(app):
+    username = "administrator"
+    password = "root"
+    if len(app.soap.get_projects(username, password)) == 0:
+        app.project.create(Project(name="test"))
+    old_projects = app.soap.get_projects(username, password)
+    project = random.choice(old_projects)
+    app.project.delete_project_by_id(project.id)
+    new_projects = app.soap.get_projects(username, password)
+    old_projects.remove(project)
+    assert old_projects == new_projects
